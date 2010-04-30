0e10517d45a7317e0f2be742b88d18b34aeddfa1
Maher Saif <maher.m.saif@gmail.com>
2010-04-30 10:18:55 +0300

    Updated to be compatable with Twig v0.9.5

--- system/application/libraries/Twig.php
+++ system/application/libraries/Twig.php
@@ -5,38 +5,50 @@ class Twig
 	private $CI;
 	private $_twig;
 	private $_template_dir;
-	private $cache_dir;
-	
+	private $_cache_dir;
+
 	/**
 	 * Constructor
 	 *
 	 */
-	function __construct()
+	function __construct($debug = false)
 	{
+
 	    $this->CI =& get_instance();
 	    $this->CI->config->load('twig');
 	    
 	    ini_set('include_path',
-		ini_get('include_path') . PATH_SEPARATOR . APPPATH . 'libraries/Twig');
-		require_once (string) "Autoloader" . EXT;
-		log_message('debug', "Twig Autoloader Loaded");
-		
-		Twig_Autoloader::register();
+            ini_get('include_path') . PATH_SEPARATOR . APPPATH . 'libraries/Twig');
+            require_once (string) "Autoloader" . EXT;
+            
+            log_message('debug', "Twig Autoloader Loaded");
 
-		$this->_template_dir = $this->CI->config->item('template_dir');
-		$this->_cache_dir = $this->CI->config->item('cache_dir');
+            Twig_Autoloader::register();
 
-		$loader = new Twig_Loader_FileSystem($this->_template_dir, $this->_cache_dir);
+            $this->_template_dir = $this->CI->config->item('template_dir');
+            $this->_cache_dir = $this->CI->config->item('cache_dir');
 
-        $this->_twig = new Twig_Environment($loader);
+            $loader = new Twig_Loader_Filesystem($this->_template_dir);
+
+            $this->_twig = new Twig_Environment($loader, array(
+                'cache' => $this->_cache_dir,
+                'debug' => $debug,
+            ));
 		
 	}
 
 	public function render($template, $data = array()) {
 
-        $template = $this->_twig->loadTemplate($template);
+            $template = $this->_twig->loadTemplate($template);
 
-        return $template->render($data);
+            return $template->render($data);
+	}
+
+        public function display($template, $data = array()) {
+
+            $template = $this->_twig->loadTemplate($template);
+
+            $template->display($data);
 	}
 }
 
